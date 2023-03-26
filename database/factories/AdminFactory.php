8fba863ae0373cbb33a831e076845fedd32f591b
hdood <m.bouguerzi1234@gmail.com>
2023-03-26 21:58:40 +0100

    initial

---
+++ database/factories/AdminFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Admin>
+ */
+class AdminFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "name" => fake()->name(),
+            "gender" => fake()->randomElement(['male', "female"]),
+            "email" => fake()->email(),
+            "password" => Hash::make("password"),
+            "phone" => "06" . fake()->randomNumber(8),
+            "address" => fake()->address()
+        ];
+    }
+}
